74772b45683940715cb7b9f3438d0d508ab2a433
Shafik46 <123635827+Shafik46@users.noreply.github.com>
2023-05-02 18:52:49 +0100

    Added Staging table and final table DML

--- code/SQL/EV_Population_DML.sql
+++ code/SQL/EV_Population_DML.sql
@@ -20,3 +20,44 @@ CREATE TABLE "DAP_Project"."Electric_Vehicles".EV_Population (
   Vehicle_Location TEXT,
   WAOFM_GIS_Legislative_District_Boundary TEXT
 );
+
+create table "DAP_Project"."Electric_Vehicles".ev_population_staging as
+            SELECT vin,census_tract,
+            base_msrp, city,
+            clean_alternative_fuel_vehicle_eligibility,
+            counties, county,
+            dol_vehicle_id,
+            electric_range,
+            COALESCE(electric_utility,'N/A') electric_utility,
+            electric_vehicle_type,
+            coalesce (model,'N/A') model,
+            make,
+            model_year,
+            postal_code,
+            state
+            FROM "DAP_Project"."Electric_Vehicles".ev_population
+            where 1=2
+            ;
+
+            CREATE TABLE "DAP_Project"."Electric_Vehicles".ev_population_fin AS
+            SELECT distinct vin, census_tract,
+            base_msrp, city,
+            clean_alternative_fuel_vehicle_eligibility,
+            CASE
+                WHEN clean_alternative_fuel_vehicle_eligibility = 'Clean Alternative Fuel Vehicle Eligible' THEN 1
+                WHEN clean_alternative_fuel_vehicle_eligibility = 'Not eligible due to low battery range' THEN 0
+                ELSE 0
+            END AS eligibility_status,
+            counties, county,
+            dol_vehicle_id,
+            electric_range,
+            COALESCE(electric_utility,'N/A') electric_utility,
+            electric_vehicle_type,
+            make,
+            model,
+            model_year,
+            postal_code,
+            state
+            FROM "DAP_Project"."Electric_Vehicles".ev_population_staging
+            where 1=2
+            ;
